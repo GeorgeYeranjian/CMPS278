20f71ad4fcc4e809afaa11c6795a298cae51bb06
GeorgeYeranjian <62235897+GeorgeYeranjian@users.noreply.github.com>
2020-11-16 20:27:59 +0200

    Update Upload.php

--- Upload.php
+++ Upload.php
@@ -17,7 +17,7 @@ if (isset($_POST['submit'])){
     if(in_array($fileActualExt, $allowed)){
         if ($fileError === 0 ) {
             if ($fileSize < 100000000) {
-                $fileNameNew = uniqid('',true).".". $fileActualExt;
+                $fileNameNew = $fileName.".". $fileActualExt;
                 $fileDestination = 'Uploadedfiles/' .$fileNameNew;
 
                 move_uploaded_file($fileTmpName, $fileDestination);
